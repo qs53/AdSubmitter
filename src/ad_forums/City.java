d062848db4328bbda64939429d06db217eabadee
qusai <qusai@wso2.com>
2020-02-01 20:15:10 +0530

    Corrections

--- src/ad_forums/City.java
+++ src/ad_forums/City.java
@@ -6,7 +6,7 @@ public class City {
     static String[] top10Cities = new String[]{"New York City", "Los Angeles", "Chicago", "Houston", "Phoenix", "San Antonio", "Philadelphia", "San Diego", "Dallas", "San Jose"};
 
     static String[] top200States = new String[]{"New York", "California", "Illinois", "Texas", "Arizona", "Texas", "Pennsylvania", "California", "Texas", "California",
-            "Texas", "Florida", "Texas", "California", "North Carolina", "Ohio", "Indiana", "Washington", "Colorado", "District Of Columbia",
+            "Texas", "Florida", "Texas", "California", "North Carolina", "Ohio", "Indiana", "Washington", "Colorado", "District of Columbia",
             "Massachusetts", "Texas", "Tennessee", "Oregon", "Nevada", "Oklahoma", "Michigan", "Tennessee", "Kentucky", "Maryland",
 //            "Wisconsin", "New Mexico", "Arizona", "California", "California", "Georgia", "Arizona", "Missouri", "Florida", "Colorado",
 //            "North Carolina", "California", "Virginia", "Nebraska", "California", "Minnesota", "Florida", "Texas", "Oklahoma", "Louisiana",
